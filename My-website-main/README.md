88fddbb1c6f443be87e01cecc5f4d5688c5986aa
Marko <markobeg15@gmail.com>
2021-09-29 16:58:46 +0200

    oldportfolio

---
+++ My-website-main/README.md
@@ -0,0 +1,2 @@
+# My-website
+![webmarco](webmarco/images/webmarco.png)
---
+++ My-website-main/webmarco/Javascript/java.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($){
+    $("#hamburger-btn").click(function() {
+
+        if ($(this).hasClass("opened")) {
+            $("nav ul").slideUp();
+
+        }   else  {
+            $("nav ul").slideDown();
+                    
+        }
+        $(this).toggleClass("opened");
+         
+
+
+       
+    });
+
+    
+});
